dc360643a22bd6eafacbbaf1557118321a479565
steven ciraolo <steven.ciraolo@gmail.com>
2018-12-17 23:07:59 -0800

    added rinkeby config

--- truffle.js
+++ truffle.js
@@ -5,6 +5,14 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(process.env.SEED_WORDS, `https://rinkeby.infura.io/${process.env.INFURA_KEY}`)
+        //return new PKWalletProvider(process.env.PRIVATE_KEY, `https://rinkeby.infura.io/${process.env.INFURA_KEY}`)
+      },
+      network_id: 4,
+      //gas: 4612388 // Gas limit used for deploys
     }
   },
   solc: {
@@ -12,5 +20,5 @@ module.exports = {
       enabled: true,
       runs: 500
     }
-  } 
+  }
 };
