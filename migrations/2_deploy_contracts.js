1deca0e7dc7773bab962c0d8ed40f2d88dafcc24
Steven Ciraolo <steven.ciraolo@gmail.com>
2019-01-19 05:41:36 +0000

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ var AlgerToken = artifacts.require("AlgerToken")
 const TOKEN_CAP = 1 * 10 ** 8
 const TOKEN_ADDRESS = '0x154cc275E42ab11495CFf4ACb170f721c1bA14f4'
 module.exports = function(deployer) {
-  //deployer.deploy(Algernon, TOKEN_ADDRESS);
+  deployer.deploy(Algernon, TOKEN_ADDRESS);
 
-  deployer.deploy(AlgerToken, TOKEN_CAP);
+  //deployer.deploy(AlgerToken, TOKEN_CAP);
 };
