e6f9ce7884c87431d53491067e37c46d42984861
steven ciraolo <steven.ciraolo@gmail.com>
2019-01-27 00:37:55 -0800

    updated Token contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var Algernon = artifacts.require("Algernon");
 var AlgerToken = artifacts.require("AlgerToken")
 
 const TOKEN_CAP = 1 * 10 ** 8
-const TOKEN_ADDRESS = '0x154cc275E42ab11495CFf4ACb170f721c1bA14f4'
+const TOKEN_ADDRESS = '0xDF7623d2C57FB6023B1ACBB454afccB9D6A1b455'
 module.exports = function(deployer) {
   //deployer.deploy(Algernon, TOKEN_ADDRESS);
 
